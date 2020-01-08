b9b11e6091379f355a33107107990f039279aa33
wooj <zomdar@gmail.com>
2020-01-07 23:43:55 -0500

    editing new-release & shoes interface +

--- src/app/models/new-release.interface.ts
+++ src/app/models/new-release.interface.ts
@@ -1,9 +1,22 @@
 export interface NewRelease {
+    releaseDate: Date;
+    releaseSites: Object;
+    shoeDetails: Object;
+}
+
+export interface NewReleaseSites {
+    dropType: string;
+    siteName: string;
+    siteLink: string;
+    dropTime: string;
+}
+
+export interface NewReleaseShoeDetail {
     brand: string;
     name: string;
     subName: string;
+    realeaseSizes: number;
+    price: number;
     photoLink: string;
-    releaseDate: Date;
-    site: string;
 }
   
